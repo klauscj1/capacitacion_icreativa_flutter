6cbd910bf1d033eff4a2fda5442e19f3a6623a62
Lina Orellana <46226513+LinaOrellana@users.noreply.github.com>
2020-10-22 09:39:49 -0500

    Calculadora de Lina

---
+++ lib/ui/pages/calculadora_lina.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CalculadoraLina extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text('Calculadora Lina'),
+    ));
+  }
+}
