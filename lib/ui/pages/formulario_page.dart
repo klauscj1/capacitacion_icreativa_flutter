8bb05d294b0069f0779e68a3bdb3a7fcb8ef3a26
Claus Chocho <clauschochoe@gmail.com>
2020-10-27 10:00:12 -0500

    construyo mi formulario

--- lib/ui/pages/formulario_page.dart
+++ lib/ui/pages/formulario_page.dart
@@ -3,6 +3,7 @@ import 'package:clase_2/ui/pages/calculadora_claus.dart';
 import 'package:clase_2/ui/pages/calculadora_gabriel.dart';
 import 'package:clase_2/ui/pages/calculadora_jorge.dart';
 import 'package:clase_2/ui/pages/calculadora_lina.dart';
+import 'package:clase_2/ui/pages/formulario_claus.dart';
 import 'package:clase_2/ui/widgets/menu_item_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -43,6 +44,12 @@ class FormulariosPage extends StatelessWidget {
               leading: 'JA',
               titulo: 'Jorge Arevalo',
               subtitulo: 'Jorge Arevalo',
+            ),
+            MenuItem(
+              pagina: FormularioClaus(),
+              leading: 'FC',
+              titulo: 'Claus',
+              subtitulo: 'Claus',
             ),
           ],
         ),
